1f7bf78031221c9ad68cfb8e94cfff9bd457c18f
itzbenz <49940811+Itzbenz@users.noreply.github.com>
2022-12-02 09:14:23 +0700

    who

--- src/main/java/Premain/Main.java
+++ src/main/java/Premain/Main.java
@@ -3,25 +3,19 @@ package Premain;
 import arc.Core;
 import arc.Events;
 import arc.files.Fi;
-import arc.graphics.*;
-import arc.graphics.g2d.Draw;
-import arc.graphics.g2d.Fill;
-import arc.graphics.g2d.TextureRegion;
+import arc.graphics.Color;
+import arc.graphics.Pixmap;
+import arc.graphics.Texture;
 import arc.graphics.gl.FrameBuffer;
-import arc.graphics.gl.GLFrameBuffer;
-import arc.math.Mathf;
-import arc.math.Rand;
 import arc.scene.ui.Dialog;
 import arc.util.Log;
 import arc.util.ScreenUtils;
+import arc.util.Time;
 import mindustry.Vars;
 import mindustry.game.EventType;
 import mindustry.game.Team;
-import mindustry.gen.*;
-import mindustry.graphics.Pal;
+import mindustry.gen.Icon;
 import mindustry.mod.Mod;
-
-import static mindustry.Vars.renderer;
 
 public class Main extends Mod {
 	boolean stat;
@@ -37,39 +31,15 @@ public class Main extends Mod {
 		int w = 224;
 		int h = 224;
 
-		Events.run(EventType.Trigger.draw, ()->{
+		final long[] start = {Time.millis()};
+		Events.run(EventType.Trigger.draw, ()-> {
+			if (Time.millis() - start[0] < 1000) return;
+			;
+			start[0] = Time.millis();
 			//check if world is loaded and not paused
 			if (Vars.state.isGame() && !Vars.state.isPaused() && Vars.renderer.minimap.getTexture() != null) {
-				int x = Vars.player.tileX();
-				int y = Vars.player.tileY();
 
-				int centerX = x - w / 2;
-				int centerY = y - h / 2;
-				Pixmap pixmap, pixmap1;
-				pixmap = Vars.renderer.minimap.getPixmap().flipY();
-
-				//Draw unit here
-				for(Unit unit : Groups.unit){
-					drawEntity(centerX, centerY, pixmap, unit.tileX(), unit.tileY(), unit, unit.team() != Vars.player.team() ? 0 : 1);
-				}
-				//Draw bullets here
-				for(Bullet bullet : Groups.bullet){
-					drawEntity(centerX, centerY, pixmap, bullet.tileX(), bullet.tileY(), bullet, 2);
-				}
-
-				//PixmapIO.writePng(crop, pixmap);
-				//Crop and chop
-				pixmap1 = pixmap.crop(centerX, centerY, w, h);
-				pixmap.dispose();
-
-				//Flipping
-				pixmap = pixmap1.flipY();
-				pixmap1.dispose();
-
-				//Flush
-				PixmapIO.writePng(crop, pixmap);
-				pixmap.dispose();
-
+				ScreenUtils.saveScreenshot(crop);
 			}
 		});
 		//do you like lambda ?
@@ -85,36 +55,35 @@ public class Main extends Mod {
 				}}.show();
 			});
 		});//sike how to color bracket
-		Vars.ui.settings.game.row().table(t ->{
-			t.check("Boolean [red][Test]", s->{
+		Vars.ui.settings.game.row().table(t -> {
+			t.check("Boolean [red][Test]", s -> {
 				stat = s;//assign to global ?
-				Vars.ui.showInfo(stat+"");//toString()
+				Vars.ui.showInfo(stat + "");//toString()
 			});
 		}).growX();//stretch
 	}
 
-	private void drawEntity(int centerX, int centerY, Pixmap pixmap, int i, int i2, Teamc team, int type) {
-		int x2 = i;
-		int y2 = i2;
-		Color color = team.team().color;
+	private void drawEntity(Pixmap pixmap, int i, int i2, Team team, int type, Team player) {
+		//reduce white
+		Color color = team.color.cpy().mul(0.9f);
 
+		if (team != player) color.mul(0.8f);
 
-
-		if(type == 0){//draw triangle
-			pixmap.set(x2, y2, color);
-			pixmap.set(x2 + 1, y2, color);
-			pixmap.set(x2, y2 + 1, color);
-		}else if(type == 1){//draw rectangle
+		if (type == 0) {//draw triangle
+			pixmap.set(i, i2, color);
+			pixmap.set(i + 1, i2, color);
+			pixmap.set(i, i2 + 1, color);
+		} else if (type == 1) {//draw rectangle
 			// X X
 			// X X
 			for(int x = 0; x < 2; x++){
 				for(int y = 0; y < 2; y++){
-					pixmap.set(x2 + x, y2 + y, color);
+					pixmap.set(i + x, i2 + y, color);
 				}
 			}
 		}else if(type == 2){//draw a pixel
 			// X
-			pixmap.set(x2, y2, color);
+			pixmap.set(i, i2, color);
 
 
 		}
